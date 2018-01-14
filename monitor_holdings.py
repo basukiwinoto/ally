524a7b8e6f8a135ed08c9bb249dfcda2d7b1ee96
Basuki Winoto <baw942@g.harvard.edu>
2018-01-14 16:01:09 -0500

    add sample code

--- monitor_holdings.py
+++ monitor_holdings.py
@@ -5,23 +5,23 @@ import time
 import yaml
 
 if __name__ == '__main__':
-	logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename="monitor.log")
-	logger = logging.getLogger(__name__)
-	trader = PyTrader(logger)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename="monitor.log")
+    logger = logging.getLogger(__name__)
+    trader = PyTrader(logger)
 
-	# account config
-	with open('account.yaml') as f:
-		acct_cfg = yaml.load(f)
-	account = acct_cfg["account"]
-	auth = OAuth1(acct_cfg["auth"]["app_key"], acct_cfg["auth"]["app_secret"],
-				  acct_cfg["auth"]["token"], acct_cfg["auth"]["token_secret"])
-	trader.link_account(account, auth)
+    # account config
+    with open('account.yaml') as f:
+        acct_cfg = yaml.load(f)
+    account = acct_cfg["account"]
+    auth = OAuth1(acct_cfg["auth"]["app_key"], acct_cfg["auth"]["app_secret"],
+                  acct_cfg["auth"]["token"], acct_cfg["auth"]["token_secret"])
+    trader.link_account(account, auth)
 
-	while(True):
-		print(time.time())
-		resp = trader.get_holdings()
-		holdings = resp["accountholdings"]
-		for h in holdings:
-			if h == "holding" and not h:
-				print("{} {}".format(holdings[h]["instrument"]["sym"],holdings[h]["qty"]))
-		time.sleep(1)
+    while(True):
+        print(time.time())
+        resp = trader.get_holdings()
+        holdings = resp["accountholdings"]
+        for h in holdings:
+            if h == "holding" and not h:
+                print("{} {}".format(holdings[h]["instrument"]["sym"],holdings[h]["qty"]))
+        time.sleep(1)
